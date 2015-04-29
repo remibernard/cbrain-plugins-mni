609436d4ab70265951e6a0c74c10831ec9c9b821
Remi Bernard <remi.bernard@mail.mcgill.ca>
2015-04-29 15:38:10 -0400

    CBRAIN issue #71 - Standardize tool_config version comparison stuff
    Removal of no-longer used compare_versions method

--- cbrain_task/civet/common/civet.rb
+++ cbrain_task/civet/common/civet.rb
@@ -23,30 +23,6 @@
 # A subclass of CbrainTask to launch bet of Civet.
 class CbrainTask::Civet
 
-  # Used in order to compare CIVET version:
-  # Return -1 if v1 <  v2 for example v1 = "1.1.11" and v2 = "1.1.12"
-  # Return 0  if v1 == v2 for example v1 = "1.1.11" and v2 = "1.1.11"
-  # Return 1  if v1 >  v2 for example v1 = "1.1.12" and v2 = "1.1.11"
-  def self.compare_versions(v1,v2)
-    v1 = $1 if v1 =~ /(\d+(\.\d+){2,})/
-    v2 = $1 if v2 =~ /(\d+(\.\d+){2,})/
-    raise "Cannot extract version number for comparison" if
-      v1.blank? || v2.blank?
-
-    v1 = v1.split(".").map(&:to_i)
-    v2 = v2.split(".").map(&:to_i)
-
-    while (v1.size < v2.size) do v1.push(0) end
-    while (v2.size < v1.size) do v2.push(0) end
-
-    0.upto(v1.size-1) do |i|
-      next if v1[i] ==  v2[i]
-      return  v1[i] <=> v2[i]
-    end
-
-    return 0  # everything is equal
-  end
-
   # Some options are interdependent for example
   # when CIVET run with -no_surface all options
   # for surface treatment are ignored
--- cbrain_task/nii2mnc/common/nii2mnc.rb
+++ cbrain_task/nii2mnc/common/nii2mnc.rb
@@ -23,30 +23,6 @@
 # A subclass of CbrainTask to launch bet of Civet.
 class CbrainTask::Nii2mnc
 
-  # Used in order to compare Nii2mnc version:
-  # Return -1 if v1 <  v2 for example v1 = "1.1.11" and v2 = "1.1.12"
-  # Return 0  if v1 == v2 for example v1 = "1.1.11" and v2 = "1.1.11"
-  # Return 1  if v1 >  v2 for example v1 = "1.1.12" and v2 = "1.1.11"
-  def self.compare_versions(v1,v2)
-    v1 = $1 if v1 =~ /(\d+(\.\d+){2,})/
-    v2 = $1 if v2 =~ /(\d+(\.\d+){2,})/
-    raise "Cannot extract version number for comparison" if
-      v1.blank? || v2.blank?
-
-    v1 = v1.split(".").map(&:to_i)
-    v2 = v2.split(".").map(&:to_i)
-
-    while (v1.size < v2.size) do v1.push(0) end
-    while (v2.size < v1.size) do v2.push(0) end
-
-    0.upto(v1.size-1) do |i|
-      next if v1[i] ==  v2[i]
-      return  v1[i] <=> v2[i]
-    end
-
-    return 0  # everything is equal
-  end
-
   def identify_options_to_ignore() #:nodoc:
     ignored_options = {}
 
